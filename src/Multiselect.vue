7419bafb7867073fd2dc22d78e8b8be8642c8172
Vincent Préfontaine-Fournier <vincent.prefontaine.fournier@boreal-is.com>
2019-01-15 14:25:21 -0500

    Remove the stop in the event of the enter: take 2

--- src/Multiselect.vue
+++ src/Multiselect.vue
@@ -6,7 +6,7 @@
     @blur="searchable ? false : deactivate()"
     @keydown.self.down.prevent="pointerForward()"
     @keydown.self.up.prevent="pointerBackward()"
-    @keydown.enter.stop.self="addPointerElement($event)"
+    @keydown.enter.self="addPointerElement($event)"
     @keyup.esc="deactivate()"
     class="multiselect">
       <slot name="caret" :toggle="toggle">
@@ -52,7 +52,7 @@
           @keyup.esc="deactivate()"
           @keydown.down.prevent="pointerForward()"
           @keydown.up.prevent="pointerBackward()"
-          @keydown.enter.prevent.self="addPointerElement($event)"
+          @keydown.enter.prevent.stop.self="addPointerElement($event)"
           @keydown.delete.stop="removeLastElement()"
           class="multiselect__input"/>
         <span
